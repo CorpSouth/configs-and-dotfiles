32c63ca5335c6cb866b6858402c2aa111f8506e9
CorpSouth <96737550+CorpSouth@users.noreply.github.com>
2022-03-10 17:31:02 +0000

    Add files via upload

--- dmenu-flexipatch/patches.h
+++ dmenu-flexipatch/patches.h
@@ -17,12 +17,12 @@
  * case-insensitive -i option with a case sensitive -s option.
  * http://tools.suckless.org/dmenu/patches/case-insensitive/
  */
-#define CASEINSENSITIVE_PATCH 1
+#define CASEINSENSITIVE_PATCH 0
 
 /* This patch centers dmenu in the middle of the screen.
  * https://tools.suckless.org/dmenu/patches/center/
  */
-#define CENTER_PATCH 1
+#define CENTER_PATCH 0
 
 /* This patch enables color emoji in dmenu by removing a workaround for a BadLength error
  * in the Xft library when color glyphs are used.
@@ -92,7 +92,7 @@
 /* This patch causes dmenu to print out the current text each time a key is pressed.
  * https://tools.suckless.org/dmenu/patches/incremental/
  */
-#define INCREMENTAL_PATCH 0
+#define INCREMENTAL_PATCH 1
 
 /* This patch adds an option to provide preselected text.
  * https://tools.suckless.org/dmenu/patches/initialtext/
