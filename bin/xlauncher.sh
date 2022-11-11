4ea294c0d175af2994e194edecb470805765065c
Aaron <96737550+CorpSouth@users.noreply.github.com>
2022-11-11 02:25:13 +0000

    Update xlauncher.sh

--- bin/xlauncher.sh
+++ bin/xlauncher.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-uxterm -name xlauncher -g 60x10+300+280 -e bash -c 'cmd=$(compgen -c | sort -u | grep -v fzf | fzf); setsid -f $cmd'
+$TERMINAL -name fzf_run -g 60x10+300+280 -e bash -c 'cmd=$(compgen -c | sort -u | grep -v fzf | fzf --no-info); setsid -f $cmd'
