2c186868902bbdf40c54e1fdab2799f55b4f65fd
Aaron <96737550+CorpSouth@users.noreply.github.com>
2023-06-12 18:15:40 -0500

    Tweaks
    Tweaking the zenity dialog pitch

--- bin/filepicker.sh
+++ bin/filepicker.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-FILE=$(zenity --file-selection --title="Select a File")
+FILE=$(zenity --height="500" --width="1000" --file-selection --title="Select a File")
 
 case $? in
-         0)
-                echo "\"$FILE\" selected.";;
-         1)
-                echo "No file selected.";;
-        -1)
-                echo "An unexpected error has occurred.";;
+            0)
+               echo "\"$FILE\" selected."               ;;
+            1)
+               echo "No file selected."                 ;;
+           -1)
+               echo "An unexpected error has occurred." ;;
 esac
