ae102f39efa2e146afaa4764049b615d9cc79c5d
Aaron <96737550+CorpSouth@users.noreply.github.com>
2023-10-15 17:26:25 +0000

    Switching over to Sway/Wayland in general

--- .config/fish/conf.d/startx.fish
+++
@@ -1,9 +0,0 @@
-#########################
-#Run Startx on TYY Login#
-#########################
-
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx $XINITRC -- -keeptty
-    end
-end
