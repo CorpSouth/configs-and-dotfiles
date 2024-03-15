fdfe96f9ed21e71ca36f5e5cee4fca238c8425ce
Aaron <96737550+CorpSouth@users.noreply.github.com>
2024-03-15 01:49:09 +0000

    Fix, maybe some actual effort

--- scripts/i3lock-dpms.sh
+++ scripts/i3lock-dpms.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-#!/bin/sh
 revert() {
    xset dpms 0 0 0
 }
 trap revert HUP INT TERM
 xset +dpms dpms 5 5 5
-i3lock -c 000000 -n
+i3lock -c 000000 -i "$HOME/Pictures/Wallpapers/SiteOrigin/Mint-Y/Shattered_Bumpmap_Blue.png" -n -t -u
 revert
