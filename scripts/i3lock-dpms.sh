53cd467c4260fa55393300bc1e36f534b32a814d
Aaron <96737550+CorpSouth@users.noreply.github.com>
2024-03-15 15:30:35 +0000

    Add files via upload

--- scripts/i3lock-dpms.sh
+++ scripts/i3lock-dpms.sh
@@ -5,5 +5,5 @@ revert() {
 }
 trap revert HUP INT TERM
 xset +dpms dpms 5 5 5
-i3lock -c 000000 -i "$HOME/Pictures/Wallpapers/SiteOrigin/Mint-Y/Shattered_Bumpmap_Blue.png" -n -t -u
+i3lock -i "$HOME/Pictures/Wallpapers/SiteOrigin/Mint-Y/Shattered_Bumpmap_Blue.png" -n -t -u
 revert
