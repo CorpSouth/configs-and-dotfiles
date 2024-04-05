4ee7df3112db23a5e529e2ca89c269afe6f3ffe7
Aaron <96737550+CorpSouth@users.noreply.github.com>
2024-04-05 16:54:25 +0000

    Add files via upload

--- scripts/pipewire-init.sh
+++ scripts/pipewire-init.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-# Kill any existing pipewire instance to restore sound
-
-pkill -u "$USER" -fx /usr/bin/pipewire 1>/dev/null 2>&1
-pkill -u "$USER" -fx /usr/bin/pipewire-pulse 1>/dev/null 2>&1
-pkill -u "$USER" -fx /usr/bin/wireplumber 1>/dev/null 2>&1
+# We need to kill any existing pipewire instance to restore sound
+pkill -u "${USER}" -fx /usr/bin/pipewire-pulse 1>/dev/null 2>&1
+pkill -u "${USER}" -fx /usr/bin/pipewire-media-session 1>/dev/null 2>&1
+pkill -u "${USER}" -fx /usr/bin/wireplumber 1>/dev/null 2>&1
+pkill -u "${USER}" -fx /usr/bin/pipewire 1>/dev/null 2>&1
 
 exec /usr/bin/pipewire &
 
-# Wait for pipewire to start before attempting to start related daemons
-
-while [ "$(pgrep -f /usr/bin/pipewire)" = "" ] ; do
-   sleep 1
+# wait for pipewire to start before attempting to start related daemons
+while [ "$(pgrep -f /usr/bin/pipewire)" = "" ]; do
+        sleep 1
 done
 
-exec /usr/bin/pipewire-pulse &
+if [ -x /usr/bin/wireplumber ]; then
+	exec /usr/bin/wireplumber &
+elif [ -x /usr/bin/pipewire-media-session ]; then
+	exec /usr/bin/pipewire-media-session &
+fi
 
-exec /usr/bin/wireplumber &
+[ -f "/usr/share/pipewire/pipewire-pulse.conf" ] && exec /usr/bin/pipewire-pulse &
