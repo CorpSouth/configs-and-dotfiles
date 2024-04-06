a81e879c6536fdaf3a12178150389d57f468a0e7
Aaron <96737550+CorpSouth@users.noreply.github.com>
2024-04-06 22:35:19 +0000

    Add files via upload

--- scripts/i3lock-dpms.sh
+++ scripts/i3lock-dpms.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-WALLPAPER="$HOME/Pictures/Wallpapers/SiteOrigin/Mint-Y/Brilliant_Bumpmap_Aqua.png"
-
 revert() {
    xset dpms 0 0 0
 }
 trap revert HUP INT TERM
 i3-msg mode default
 xset +dpms dpms 5 5 5
-i3lock -i "$WALLPAPER" -n -t -u
+i3lock -c 000000 -u
 revert
